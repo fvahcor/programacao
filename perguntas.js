6daa9201d273a7e7948c0b25f6c5363e0ed66e3f
fvahcor <flavio.castro.rocha@escola.pr.gov.br>
2025-08-27 07:51:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,179 @@
+criaCartao(
+    'Esportes',
+    'Qual é o esporte mais praticado no mundo?',
+    'Futebol.'
+)
+
+criaCartao(
+    'Esportes',
+    'Em que país foram realizados os primeiros Jogos Olímpicos da era moderna?',
+    'Grécia, em 1896.'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual tenista tem mais títulos de Grand Slam no masculino?',
+    'Novak Djokovic.'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual é o recorde mundial dos 100 metros rasos no atletismo?',
+    '9,58 segundos (Usain Bolt, 2009).'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual país venceu a Copa do Mundo de 2002?',
+    'Brasil.'
+)
+
+criaCartao(
+    'Esportes',
+    'Quantos jogadores um time de vôlei tem em quadra durante o jogo?',
+    '6 Jogadores.'
+)
+
+criaCartao(
+    'Esportes',
+    'Em qual esporte se usa a expressão “strike”?',
+    'Boliche.'
+)
+
+criaCartao(
+    'Esportes',
+    'Quem é conhecido como “O Rei do Futebol”?',
+    'Pelé.'
+)
+
+criaCartao(
+    'Esportes',
+    'Em que esporte se destaca Michael Phelps?',
+    'Natação.'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual é a pontuação máxima possível em uma partida de boliche?',
+    '300 Pontos.'
+)
+
+criaCartao(
+    'Esportes',
+    'Quantos rounds tem uma luta de boxe profissional (peso pesado)?',
+    '12 Rouds.'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual é o esporte nacional do Japão?',
+    'Sumo.'
+)
+
+criaCartao(
+    'Esportes',
+    'Quem foi o maior artilheiro da Copa do Mundo de 2002?',
+    'Ronaldo Fenômeno.'
+)
+
+criaCartao(
+    'Esportes',
+    'No basquete, quantos pontos vale Quando é feito um dunk?',
+    '2 Pontos.'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual seleção venceu a Copa do Mundo de 2018?',
+    'França.'
+)
+
+criaCartao(
+    'Automóveis',
+    'Qual país é a origem da marca Ferrari?',
+    'Italía.'
+)
+
+criaCartao(
+    'Automóveis',
+    'O que significa a sigla ABS nos carros?',
+    'Anti-lock Braking System (Sistema de freios antitravamento).'
+)
+
+criaCartao(
+    'Automóveis',
+    'Quem foi o primeiro campeão da Fórmula 1, em 1950?',
+    'Giuseppe Farina.'
+)
+
+criaCartao(
+    'Automóveis',
+    'Qual é o carro mais vendido da história?',
+    'Toyota Corolla.'
+)
+
+criaCartao(
+    'Automóveis',
+    'Qual é a função do turbo em um motor?',
+    'Aumentar a potência, comprimindo mais ar na câmara de combustão.'
+)
+
+criaCartao(
+    'Automóveis',
+    'O que significa RPM no painel do carro?',
+    'Rotações Por Minuto.'
+)
+
+criaCartao(
+    'Automóveis',
+    'Qual marca produz o modelo Mustang?',
+    'Ford.'
+)
+
+criaCartao(
+    'Automóveis',
+    'Quem é o maior campeão da Fórmula 1 em títulos mundiais?',
+    'Lewis Hamilton e Michael Schumacher (7 títulos cada).'
+)
+
+criaCartao(
+    'Automóveis',
+    'Em que país foi criada a Lamborghini?',
+    'Italía.'
+)
+
+criaCartao(
+    'Automóveis',
+    'Qual é a velocidade máxima oficial de um Bugatti Chiron?',
+    'Cerca de 420 km/h.'
+)
+
+criaCartao(
+    'Automóveis',
+    'O que significa SUV?',
+    'Sport Utility Vehicle (Veículo utilitário esportivo).'
+)
+
+criaCartao(
+    'Automóveis',
+    'Qual marca fabrica o modelo Civic?',
+    'Honda.'
+)
+
+criaCartao(
+    'Automóveis',
+    'Em que ano foi lançado o primeiro carro popular no Brasil, o Fusca?',
+    '1959(Produção nacional).'
+)
+
+criaCartao(
+    'Automóveis',
+    'Qual piloto brasileiro foi campeão da Fórmula 1 em 1981?',
+    'Nelson Piquet.'
+)
+
+criaCartao(
+    'Automóveis',
+    'Qual peça do carro é responsável por iniciar a combustão no motor?',
+    'Vela de ignição.'
+)
